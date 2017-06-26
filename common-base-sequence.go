2e3dce6b29f3e2f29bb47af700387355ec408e73
Luke Sikina <luke@genomoncology.com>
2017-06-26 09:57:14 -0400

    Ugly implmentation, but it should be very memory efficient.

--- common-base-sequence.go
+++ common-base-sequence.go
@@ -1,20 +1,65 @@
 package main
 
-type Sequence [16]rune
+import (
+	"bufio"
+	"io"
+	"log"
+	"strconv"
+)
 
-func (sequence *Sequence) hash() (hash uint32) {
-	hash = 0
-	for _, char := range sequence {
-		switch char {
-		case 'A':
-			hash = hash << 2
-		case 'G':
-			hash = hash<<2 + 1
-		case 'C':
-			hash = hash<<2 + 2
-		default:
-			hash = hash<<2 + 3
+func GatherCommonSequences(stream *bufio.Reader) (frequencies map[uint32]uint) {
+	var validTokenSequence uint16 = 0
+	var hash uint32 = 0
+	frequencies = make(map[uint32]uint)
+	for {
+		if c, _, err := stream.ReadRune(); err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				log.Fatal(err)
+			}
+		} else {
+			log.Print(strconv.QuoteRune(c))
+			skip := false
+			switch c {
+			case 'A':
+				hash = hash << 2
+				validTokenSequence = (validTokenSequence << 1) + 1
+			case 'G':
+				hash = (hash << 2) + 1
+				validTokenSequence = (validTokenSequence << 1) + 1
+			case 'C':
+				hash = (hash << 2) + 2
+				validTokenSequence = (validTokenSequence << 1) + 1
+			case 'T':
+				hash = (hash << 2) + 3
+				validTokenSequence = (validTokenSequence << 1) + 1
+			case '\n':
+				skip = true
+			default:
+				validTokenSequence = validTokenSequence << 1
+			}
+			if validTokenSequence == 65535 && !skip {
+				frequencies[hash]++
+			}
 		}
+	}
+	return
+}
+
+func ReverseHash(hash uint32) (bases string) {
+	for i := 0; i < 16; i++ {
+		switch hash % 4 {
+		case 0:
+			bases = "A" + bases
+		case 1:
+			bases = "G" + bases
+		case 2:
+			bases = "C" + bases
+		case 3:
+			bases = "T" + bases
+		}
+		hash = hash >> 2
 	}
 	return
 }
--- common-base-sequence_test.go
+++ common-base-sequence_test.go
@@ -1,12 +1,42 @@
 package main
 
 import (
+	"bufio"
 	"github.com/stretchr/testify/assert"
+	"strings"
 	"testing"
 )
 
-func TestHash(t *testing.T) {
-	sequence := Sequence{'A', 'C', 'G', 'T', 'A', 'C', 'G', 'T', 'A', 'C', 'G', 'T', 'A', 'C', 'G', 'T'}
-	actual := sequence.hash()
-	assert.Equal(t, uint32(656877351), actual)
+func TestGatherCommonSequences(t *testing.T) {
+	sequence := "CCTAACCCTAACCCTAACCTAACCCTAACCCTA\n"
+	expected := make(map[string]uint)
+	expected["CCTAACCCTAACCCTA"] = 2
+	expected["CTAACCCTAACCCTAA"] = 1
+	expected["TAACCCTAACCCTAAC"] = 1
+	expected["AACCCTAACCCTAACC"] = 1
+	expected["ACCCTAACCCTAACCT"] = 1
+	expected["CCCTAACCCTAACCTA"] = 1
+	expected["CCTAACCCTAACCTAA"] = 1
+	expected["CTAACCCTAACCTAAC"] = 1
+	expected["TAACCCTAACCTAACC"] = 1
+	expected["AACCCTAACCTAACCC"] = 1
+	expected["ACCCTAACCTAACCCT"] = 1
+	expected["CCCTAACCTAACCCTA"] = 1
+	expected["CCTAACCTAACCCTAA"] = 1
+	expected["CTAACCTAACCCTAAC"] = 1
+	expected["TAACCTAACCCTAACC"] = 1
+	expected["AACCTAACCCTAACCC"] = 1
+	expected["ACCTAACCCTAACCCT"] = 1
+
+	counts := GatherCommonSequences(bufio.NewReader(strings.NewReader(sequence)))
+	for hash, count := range counts {
+		subSequence := ReverseHash(hash)
+		assert.Equal(t, expected[subSequence], count)
+	}
+}
+
+func TestReverseHash(t *testing.T) {
+	expected := "AGCTAGCTAGCTAGCT"
+	actual := ReverseHash(454761243)
+	assert.Equal(t, expected, actual)
 }
